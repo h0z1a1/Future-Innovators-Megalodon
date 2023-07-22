f842f9f4cc8209733b6b2d84841b8d657f65a575
h0z1a1 <137758764+h0z1a1@users.noreply.github.com>
2023-07-22 22:58:40 +0300

    Add files via upload

---
+++ code/LDR_CODE.ino
@@ -0,0 +1,92 @@
+//THIS CODE IS FOR THE CONTROL HOUSE(PUSH BUTTON,LCD,AND LDR)
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);//LCD
+volatile boolean x;
+
+void close() {
+  analogWrite(10,70);
+  digitalWrite(11,HIGH);
+  digitalWrite(12,LOW);
+}
+
+void open() {
+  analogWrite(10,70);
+  digitalWrite(11,LOW);
+  digitalWrite(12,HIGH);
+}
+
+void stop() {
+  analogWrite(10,0);
+  digitalWrite(11,LOW);
+  digitalWrite(12,LOW);
+}
+
+void setup(){
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);//LCD
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+  Serial.begin(9600);
+  pinMode(8, OUTPUT);//LED
+  pinMode(3, OUTPUT);//LED
+  pinMode(4, OUTPUT);//LED
+  pinMode(5, OUTPUT);//LED
+  pinMode(6, OUTPUT);//LED
+  pinMode(7, OUTPUT);//LED
+  pinMode(11, OUTPUT);//MOTOR
+  pinMode(12, OUTPUT);//MOTOR
+  pinMode(13, INPUT);//PUSH BUTTON
+  x = 0;
+}
+
+void loop(){
+  display.setTextSize(0.96);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.println("Hello everyone");
+  display.println("I'am Megalodon");
+  display.println("built by bot-builders");
+  display.println("let's goooo");
+  display.display();
+  delay(250);
+  Serial.println(analogRead(A1));
+  if (analogRead(A1) <= 500) {
+    digitalWrite(8,HIGH);
+    digitalWrite(3,HIGH);
+    digitalWrite(4,HIGH);
+    digitalWrite(5,HIGH);
+    digitalWrite(6,HIGH);
+    digitalWrite(7,HIGH);
+
+  } else {
+    digitalWrite(8,LOW);
+    digitalWrite(3,LOW);
+    digitalWrite(4,LOW);
+    digitalWrite(5,LOW);
+    digitalWrite(6,LOW);
+    digitalWrite(7,LOW);
+
+  }
+
+  if (digitalRead(13) == HIGH) {
+    x = !x;
+    delay(1000);
+    if (x == 1) {
+      open();
+      delay(550);
+      stop();
+
+    }
+    if (x == 0) {
+      close();
+      delay(550);
+      stop();
+
+    }
+
+  }
+
+}
---
+++ code/MEGALODON.ino
@@ -0,0 +1,94 @@
+//THIS CODE IS FOR THE ROBOT'S ROLL AND DRIVING
+int U;//VARIABLE FOR ULTRASONIC SENSOR
+int W;//VARIABLE FOR WATER SENSOR
+int T;//VARIABLE FOR TURBIDITY SENSOR
+
+float checkdistance_6_5() {
+  digitalWrite(6, LOW);
+  delayMicroseconds(2);
+  digitalWrite(6, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(6, LOW);
+  float distance = pulseIn(5, HIGH) / 58.00;
+  delay(10);
+  return distance;
+}
+
+void forward() {
+  analogWrite(3,80);
+  digitalWrite(4,HIGH);
+  digitalWrite(7,LOW);
+  analogWrite(11,80);
+  digitalWrite(12,LOW);
+  digitalWrite(13,HIGH);
+}
+
+void left() {
+  analogWrite(3,80);
+  digitalWrite(4,LOW);
+  digitalWrite(7,HIGH);
+  analogWrite(11,80);
+  digitalWrite(12,LOW);
+  digitalWrite(13,HIGH);
+}
+
+void stop() {
+  analogWrite(3,0);
+  digitalWrite(4,LOW);
+  digitalWrite(7,LOW);
+  analogWrite(11,0);
+  digitalWrite(12,LOW);
+  digitalWrite(13,LOW);
+}
+
+void right() {
+  analogWrite(3,80);
+  digitalWrite(4,LOW);
+  digitalWrite(7,HIGH);
+  analogWrite(11,80);
+  digitalWrite(12,HIGH);
+  digitalWrite(13,LOW);
+}
+
+void setup(){
+  U = 0;
+  W = 0;
+  T = 0;
+  pinMode(6, OUTPUT);//ULTRASONIC TRIG
+  pinMode(5, INPUT);//ULTRASONIC ECHO 
+  Serial.begin(9600);
+  pinMode(8, OUTPUT);//DELAY
+  pinMode(3,OUTPUT);//ANALOG LEFT SIDE
+  pinMode(4, OUTPUT);//LEFT MOTOR
+  pinMode(7, OUTPUT);//LEFT MOTOR
+  pinMode(11,OUTPUT);//ANALOG RIGHT SIDE
+  pinMode(12, OUTPUT);//RIGHT MOTOR
+  pinMode(13, OUTPUT);//RIGHT MOTOR
+}
+
+void loop(){
+  U = checkdistance_6_5();
+  W = analogRead(A5);
+  T = analogRead(A0);
+  Serial.println(String("ULTRA :") + String(U));
+  Serial.println(String("WATER : ") + String(W));
+  Serial.println(String("Turbidity") + String(T));
+  if (U > 15) {
+    forward();
+
+  } else if (U < 15) {
+    left();
+  }
+  if (W < 305 && T >= 780) {
+    digitalWrite(8,LOW);
+
+  } else if (W > 305 && T >= 780) {
+    digitalWrite(8,HIGH);
+  } else if (W > 305 && T < 780) {
+    digitalWrite(8,HIGH);
+  } else {
+    digitalWrite(8,HIGH);
+
+  }
+
+}
